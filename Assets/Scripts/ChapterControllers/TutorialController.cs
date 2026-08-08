66462675a6d559934860886d14fcd231ea83967c
lauura04 <laurafacendansr@gmail.com>
2026-08-08 02:57:19 +0200

    comienzo paso de fases

--- Assets/Scripts/ChapterControllers/TutorialController.cs
+++ Assets/Scripts/ChapterControllers/TutorialController.cs
@@ -6,7 +6,17 @@ using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
-    
+    public static TutorialController Instance {get;private set;}
+
+    public enum TutorialPhase
+    {
+        Intro,
+        GiftIdea,
+        FindMaterials,
+        End
+    }
+
+    public TutorialPhase CurrentPhase {get; private set;}
     [SerializeField] private DialogueData initialDialogue;
 
     private void Start()
